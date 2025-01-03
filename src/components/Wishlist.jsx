ebb172510402b8cbd89b12237543cbdcb85008f8
SolCaruso <caruso@ocelot.ltd>
2025-01-03 06:34:30 -0500

    Fix WishList import path

--- src/components/Wishlist.jsx
+++ src/components/Wishlist.jsx
@@ -6,12 +6,9 @@ import styles from "./WishList.module.css";
 export default function WishList() {
   return (
     <div className="flex justify-center mt-8">
-      
       <div className={`${styles.wishlistOuter} ${styles.wishlistAnimate}`}>
-       
         <div className={styles.wishlistAnnouncement}>
           <span>Wishlist now open</span>
-
           <span className={styles.wishlistMuted}>
             Join wishlist
             <RightArrowIcon />
