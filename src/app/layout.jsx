78c85c19a23e31fbd06517204d537e9053c431ad
SolCaruso <caruso@ocelot.ltd>
2025-01-03 05:06:25 -0500

    cleans up file paths

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -5,8 +5,8 @@ import '@/styles/tailwind.css'
 
 export const metadata = {
   title: {
-    template: '%s - Protocol API Reference',
-    default: 'Protocol API Reference',
+    template: 'Revolver.ag',
+    default: 'Revolver.ag',
   },
 }
 
