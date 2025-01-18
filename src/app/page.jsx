f3c17348163833dd266291c978af29e91af5b498
SolCaruso <caruso@ocelot.ltd>
2025-01-18 04:16:17 -0500

    reverts screenshot. fix later

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,7 +1,34 @@
 import LandingLayout from "@/components/LandingLayout";
 import Hero from "@/components/Hero";
 import CardSection from "@/components/CardSection";
-import { FeatureSection } from "@/components/FeatureSection";
+import SportsbookShowcase from "@/components/SportsbookShowcase";
+import { Container } from '@/components/container'
+import { Screenshot } from '@/components/screenshot'
+import { Heading } from '@/components/text'
+import { StarsContainer } from '@/components/StarsContainer'
+
+
+function FeatureSection() {
+  return (
+    <div className="overflow-hidden">
+      <Container className="pb-24">
+        <Heading as="h2" className="max-w-3xl">
+        The best odds and highest limits on Solana.
+        </Heading>
+        <div className="relative">
+          <StarsContainer />
+          <Screenshot
+            width={1216}
+            height={768}
+            src="/images/webp/app.webp"
+            className="mt-16 h-[36rem] sm:h-auto sm:w-[76rem]"
+          />
+        </div>
+      </Container>
+      
+    </div>
+  )
+}
 
 export default function Home() {
   return (
